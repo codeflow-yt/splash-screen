f6e0d2739662d512065217c3789e934060fdf6c8
wekonu <wahjoe.eko.n@gmail.com>
2021-05-24 21:56:47 +0700

    [MAJOR ADD] initialize project files

---
+++ Splash/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Splash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter >= 1000)
+            {
+                timer1.Enabled = false;
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+        }
+    }
+}
